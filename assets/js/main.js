e0786d530e41b85b1b42927542714682a3b697ab
Graph1te <egor.anders23@gmail.com>
2023-01-14 22:29:51 +0500

    tablet darkmode func

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,7 +43,6 @@ document.addEventListener('DOMContentLoaded', () => {
       }
 
       starsWrapper.classList.toggle('rating--active');
-
     });
 
     starsArr.forEach((star) => {
@@ -56,25 +55,89 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   //dark-mode
-
-  const darkButton = document.querySelector('.info__theme--dark');
-  const lightButton = document.querySelector('.info__theme--light');
-
-  darkButton.addEventListener('click', ()=> {
-    darkButton.classList.add('info__theme--active');
-    lightButton.classList.remove('info__theme--active');
-    document.body.classList.add('dark-mode');
-    document.querySelector('.content__discover').classList.add('content__discover--dark');
-    document.querySelector('.info__balance--light').classList.remove('info__balance--active');
-    document.querySelector('.info__balance--dark').classList.add('info__balance--active');
-  });
-
-  lightButton.addEventListener('click', ()=> {
-    darkButton.classList.remove('info__theme--active');
-    lightButton.classList.add('info__theme--active');
-    document.body.classList.remove('dark-mode');
-    document.querySelector('.content__discover').classList.remove('content__discover--dark');
-    document.querySelector('.info__balance--light').classList.add('info__balance--active');
-    document.querySelector('.info__balance--dark').classList.remove('info__balance--active');
-  });
 });
+
+const darkButton = document.querySelector('.info__theme--dark');
+const lightButton = document.querySelector('.info__theme--light');
+
+darkButton.addEventListener('click', () => {
+  darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+  enableDarkMode();
+});
+
+lightButton.addEventListener('click', () => {
+  disableDarkMode();
+});
+
+
+let darkMode = localStorage.getItem("dark-mode");
+
+const enableDarkMode = () => {
+  darkButton.classList.add('info__theme--active');
+  lightButton.classList.remove('info__theme--active');
+  darkButtonMobile.classList.add('mobile__theme--active');
+  lightButtonMobile.classList.remove('mobile__theme--active');
+  document.body.classList.add('dark-mode');
+  document.querySelector('.content__discover').classList.add('content__discover--dark');
+  document.querySelector('.info__balance--light').classList.remove('info__balance--active');
+  document.querySelector('.info__balance--dark').classList.add('info__balance--active');
+  localStorage.setItem("dark-mode", "enabled");
+};
+
+const disableDarkMode = () => {
+  darkButton.classList.remove('info__theme--active');
+  lightButton.classList.add('info__theme--active');
+  darkButtonMobile.classList.remove('mobile__theme--active');
+  lightButtonMobile.classList.add('mobile__theme--active');
+  document.body.classList.remove('dark-mode');
+  document.querySelector('.content__discover').classList.remove('content__discover--dark');
+  document.querySelector('.info__balance--light').classList.add('info__balance--active');
+  document.querySelector('.info__balance--dark').classList.remove('info__balance--active');
+  localStorage.setItem("dark-mode", "disabled");
+
+};
+
+
+
+const darkButtonMobile = document.querySelector('.mobile__theme--dark');
+const lightButtonMobile = document.querySelector('.mobile__theme--light');
+
+darkButtonMobile.addEventListener('click', () => {
+  darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+  enableDarkModeMobile();
+});
+
+lightButtonMobile.addEventListener('click', () => {
+  disableDarkModeMobile();
+});
+
+const enableDarkModeMobile = () => {
+  darkButton.classList.add('info__theme--active');
+  lightButton.classList.remove('info__theme--active');
+  darkButtonMobile.classList.add('mobile__theme--active');
+  lightButtonMobile.classList.remove('mobile__theme--active');
+  document.body.classList.add('dark-mode');
+  document.querySelector('.content__discover').classList.add('content__discover--dark');
+  document.querySelector('.info__balance--light').classList.remove('info__balance--active');
+  document.querySelector('.info__balance--dark').classList.add('info__balance--active');
+  localStorage.setItem("dark-mode", "enabled");
+};
+
+const disableDarkModeMobile = () => {
+  darkButton.classList.remove('info__theme--active');
+  lightButton.classList.add('info__theme--active');
+  darkButtonMobile.classList.remove('mobile__theme--active');
+  lightButtonMobile.classList.add('mobile__theme--active');
+  document.body.classList.remove('dark-mode');
+  document.querySelector('.content__discover').classList.remove('content__discover--dark');
+  document.querySelector('.info__balance--light').classList.add('info__balance--active');
+  document.querySelector('.info__balance--dark').classList.remove('info__balance--active');
+  localStorage.setItem("dark-mode", "disabled");
+};
+
+
+if (darkMode === "enabled") {
+  enableDarkMode(); // set state of darkMode on page load
+  enableDarkModeMobile();
+}
+
